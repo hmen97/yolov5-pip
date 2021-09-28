e6cc775211fd917cfb81c212ea19c5ddd26dda59
hmen97 <hmenezes36@gmail.com>
2021-09-29 00:30:19 +0530

    changed cli routing to facilitate install options

--- yolov5/cli.py
+++ yolov5/cli.py
@@ -6,40 +6,40 @@ from yolov5.detect import run as yolov5_detect
 from yolov5.export import run as yolov5_export
 
 
-# def train() -> None:
-#     fire.Fire(
-#         {
-#             "train": yolov5_train,
-#         }
-#     )
-
-# def val() -> None:
-#     fire.Fire(
-#         {
-#             "val": yolov5_val,
-#         }
-#     )
-
-# def detect() -> None:
-#     fire.Fire(
-#         {
-#             "detect": yolov5_detect,
-#         }
-#     )
-
-# def export() -> None:
-#     fire.Fire(
-#         {
-#             "export": yolov5_export,
-#         }
-#     )
-
-def app() -> None:
+def train() -> None:
     fire.Fire(
         {
             "train": yolov5_train,
+        }
+    )
+
+def val() -> None:
+    fire.Fire(
+        {
             "val": yolov5_val,
+        }
+    )
+
+def detect() -> None:
+    fire.Fire(
+        {
             "detect": yolov5_detect,
+        }
+    )
+
+def export() -> None:
+    fire.Fire(
+        {
             "export": yolov5_export,
         }
     )
+
+# def app() -> None:
+#     fire.Fire(
+#         {
+#             "train": yolov5_train,
+#             "val": yolov5_val,
+#             "detect": yolov5_detect,
+#             "export": yolov5_export,
+#         }
+#     )
